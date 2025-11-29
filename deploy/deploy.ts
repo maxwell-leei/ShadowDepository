11f04c010170236134e3e8494f9fe63be97d4987
maxwell-leei <nkfdtnqm081@outlook.com>
2025-11-29 12:04:44 +0800

    deploy contract

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const registry = await deploy("Shadowepository", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`Shadowepository contract: ${registry.address}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_shadowepository";
+func.tags = ["Shadowepository"];
